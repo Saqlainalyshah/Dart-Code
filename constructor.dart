85f5a50590471cda796930f9aab079254a759428
Saqlainalyshah <saqlainalishah.official@gmail.com>
2025-04-19 16:03:17 +0500

    Constructor are special functions use to initialize values for the object

--- constructor.dart
+++ constructor.dart
@@ -5,11 +5,29 @@ class Person{
 //Constructor
 //A constructor is a special method used to initialize an object.
 // It is called automatically when an object is created, and it can be used to set the initial values for the object’s properties
+///Things To Remember
+     ///The constructor’s name should be the same as the class name.
+    ///Constructor doesn’t have any return type.
 Person(this.name,this.age);
+
 
 display(){
   print("Name is $name and age is $age");
 }
+}
+class Student {
+  String? name;
+  int? age;
+  int? rollNumber;
+
+  // Constructor
+  Student(String name, int age, int rollNumber) {
+    print(
+        "Constructor called"); // this is for checking the constructor is called or not.
+    this.name = name;
+    this.age = age;
+    this.rollNumber = rollNumber;
+  }
 }
 
 void main(){
@@ -22,5 +40,11 @@ void main(){
   person.name="Saqlain";
   person.age=50;
   person.display();// output Name is Saqlain and age is 25
+
+   // Here student is object of class Student.
+  Student student = Student("John", 20, 1);
+  print("Name: ${student.name}");
+  print("Age: ${student.age}");
+  print("Roll Number: ${student.rollNumber}");
 
 }
